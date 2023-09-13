e5c982ccd2e8f8b56d00553f7754054195f42f89
Rose Wright <rosesophiewright@gmail.com>
2023-09-12 22:04:45 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
+function Cup({ guest }) {
+  return <h2>Tea cup for the guest #{guest}</h2>
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let cups = [];
+
+  for (let i = 1; i <= 12; i++) {
+    cups.push(<Cup key={i} guest={i} />)
+  }
+  return cups;
 }
 
 export default App;
